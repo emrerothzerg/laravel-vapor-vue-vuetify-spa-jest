af07649a98aa4ae7c62772e829ec72dcff21c3b2
Emre Rothzerg <hello@emrerothzerg.com>
2021-05-02 17:57:48 +1000

    assets fix

--- resources/js/components/Logo.vue
+++ resources/js/components/Logo.vue
@@ -1,3 +1,14 @@
 <template>
-    <img :src="'../../img/emre_logo.jpeg'" height="200" />
+    <img :src="image" height="200" />
 </template>
+<script>
+import image from "../../img/emre_logo.jpeg";
+
+export default {
+    data: function() {
+        return {
+            image: image
+        };
+    }
+};
+</script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,3 +4,7 @@ require("vuetifyjs-mix-extension");
 mix.js("resources/js/laravelemployer/appEmployer.js", "public/js").vue();
 mix.js("resources/js/laraveladmin/appAdmin.js", "public/js").vue();
 mix.js("resources/js/laravelwww/appWWW.js", "public/js").vue();
+
+const ASSET_URL = `${process.env.ASSET_URL || ""}/`;
+
+mix.setResourceRoot(ASSET_URL);
